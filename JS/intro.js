0ed61472c83accd5b007fb832d77dbd709f06028
goelabhishek694 <goelabhishek694@gmail.com>
2022-06-10 23:44:13 +0530

    :sparkles: js basics

---
+++ JS/intro.js
@@ -0,0 +1,39 @@
+// variable declaration 
+var a;
+
+// varibale initialisation
+a = 10;
+
+//variable reinitialisation
+a = 20;
+
+//print
+console.log(a);
+
+console.log(typeof a);
+
+a = "hello";
+console.log(typeof a);
+
+a = true;
+console.log(typeof a);
+
+
+a = null;
+console.log(typeof a);
+
+// data types in javascript
+
+// 1) Number
+// 2) Bigint
+// 3) Boolean
+// 4) String
+// 5) null
+// 6) undefined
+// 7) objects
+// 8) symbol
+
+
+
+
+
