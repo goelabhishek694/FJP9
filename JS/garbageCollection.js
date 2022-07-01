28ba003e18ea40296cbd4d6451db592a3e6b2bb4
goelabhishek694 <goelabhishek694@gmail.com>
2022-07-02 00:07:15 +0530

    :construction: garbageCollection, setTimeout

---
+++ JS/garbageCollection.js
@@ -0,0 +1,15 @@
+function marry(man, woman) {
+  woman.husband = man;
+  man.wife = woman;
+
+  return {
+    father: man,
+    mother: woman
+  }
+}
+
+let family = marry({
+  name: "John"
+}, {
+  name: "Ann"
+});
---
+++ JS/setTimeout.js
@@ -0,0 +1,45 @@
+// function a() {
+//     var b = 1;
+//     setTimeout(function () {
+//         console.log(b);
+//     },3000);
+//     console.log("Hello");
+// };
+
+// a();
+
+//interview ques
+// print number from 1 to 5 , with a second gap in each number
+
+// function a() {
+//     for (let i = 1; i <= 5; i++) {
+//         // setTimeout(function () {
+//         //     console.log(i);
+//         // }, 1000*i);
+//         console.log(i);
+//     }
+// }
+// a(); //1 2 3 4 5
+
+// function a() {
+//   for (var i = 1; i <= 5; i++) {
+//     setTimeout(function () {
+//       console.log(i);
+//     }, 1000 * i);
+//   }
+// }
+// a(); // 6 6 6 6 6
+
+// function a() {
+    
+//     for (var i = 1; i <= 5; i++) {
+//         function wrapper(b) {
+//             setTimeout(function () {
+//                 console.log(b);
+//             }, 1000 * b);
+//         }
+//         wrapper(i);
+//     }
+// }
+// a(); //1 2 3 4 5
+
