cce871b470d232bdec37b57641ab39feb6d0f439
goelabhishek694 <goelabhishek694@gmail.com>
2022-07-25 23:09:08 +0530

    :construction: ticketCont , uid

--- Browser/jira_clone/script.js
+++ Browser/jira_clone/script.js
@@ -1,27 +1,59 @@
+var uid = new ShortUniqueId();
 // Returns the first element that matches selectors.
-let addBtn = document.querySelector(".add-btn");
-let modalCont = document.querySelector(".modal-cont");
+const addBtn = document.querySelector(".add-btn");
+const modalCont = document.querySelector(".modal-cont");
+const textArea = document.querySelector(".textarea-cont");
 // console.log(modalCont);
-
+const colors = ["lightpink", "lightgreen", "lightblue", "black"];
+let modalPriorityColor = colors[colors.length - 1]; //black
+const mainCont = document.querySelector(".main-cont");
 
 var isModalPresent = false;
-addBtn.addEventListener("click", function () {
-   
-//    case 1 -> if modal is not present
-//              then display modal
-    if (!isModalPresent) { 
-        // display modal
-        modalCont.style.display = "flex";
-    }
-    
-    
-    // case 2 -> if modal is Present
-    //           then hide modal
+addBtn.addEventListener("click", function (e) {
+  console.log(e);
+  //    case 1 -> if modal is not present
+  //              then display modal
+  if (!isModalPresent) {
+    // display modal
+    modalCont.style.display = "flex";
+  }
 
-    else if(isModalPresent) {
-        // display none
-        modalCont.style.display = "none";
-    }
+  // case 2 -> if modal is Present
+  //           then hide modal
+  else if (isModalPresent) {
+    // display none
+    modalCont.style.display = "none";
+  }
 
-    isModalPresent = !isModalPresent;
-})
+  isModalPresent = !isModalPresent;
+});
+
+modalCont.addEventListener("keydown", function (e) {
+  console.log(e);
+  if (e.key == "Shift") {
+    //1 call createTicket()
+    console.log(textArea.value);
+    createTicket(modalPriorityColor, textArea.value);
+    //2 alter display and update isModalPresent
+    modalCont.style.display = "none";
+    isModalPresent = false;
+    textArea.value = "";
+  }
+});
+
+function createTicket(ticketColor, data) {
+  //generate uid
+  let id = uid();
+
+  let ticketCont = document.createElement("div");
+  ticketCont.setAttribute("class", "ticket-cont");
+  ticketCont.innerHTML = `
+        <div class="ticket-color ${ticketColor}"></div>
+        <div class="ticket-id">#${id}</div>
+        <div class="task-area">${data}</div>
+        <div class="ticket-lock">
+            <i class="fa-solid fa-lock"></i>
+        </div>
+    `;
+  mainCont.appendChild(ticketCont);
+}
