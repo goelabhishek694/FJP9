c2a3449e7ff483d17e0325180df658730785aaf4
goelabhishek694 <goelabhishek694@gmail.com>
2022-08-08 23:49:32 +0530

    :construction:

--- Browser/Camera/script.js
+++ Browser/Camera/script.js
@@ -4,6 +4,61 @@ let captureBtn=document.querySelector(".capture-btn")
 let captureBtnCont=document.querySelector(".capture-btn-cont")
 let timerCont = document.querySelector(".timer-cont");
 let timer = document.querySelector(".timer");
+let video = document.querySelector("video");
+
+let constraints = {
+    video: true,
+    audio: true
+}
+let mediaRecorder;
+//this will store the video mediastream 
+let chunks = [];
+navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
+    video.srcObject = stream;
+
+    mediaRecorder = new MediaRecorder(stream);
+
+    mediaRecorder.addEventListener("start", () => {
+        console.log("rec started");
+    });
+
+    mediaRecorder.addEventListener("dataavailable", (e) => {
+        chunks.push(e.data);
+        
+    })
+
+     mediaRecorder.addEventListener("stop", () => {
+         console.log("rec stopped");
+         let blob = new Blob(chunks, { type: "video/mp4" });
+         let videoURL = URL.createObjectURL(blob);
+         console.log(videoURL);
+
+         let a = document.createElement("a");
+         a.href = videoURL;
+         a.download="myVideo.mp4"
+         a.click();
+     });
+})
+
+let isRecording = false;
+recordBtnCont.addEventListener("click", function () {
+    if (!isRecording) {
+        //we have to record 
+         mediaRecorder.start();
+        recordBtn.classList.add("scale-record");
+        timer.style.display = "block";
+    } else {
+        //stop the recording 
+        mediaRecorder.stop();
+        recordBtn.classList.remove("scale-record");
+        timer.style.display = "none";
+    }
+    
+    isRecording = !isRecording;
+  
+});
+
+
 
 captureBtnCont.addEventListener("click", function(){
     captureBtn.classList.add("scale-capture");
@@ -11,20 +66,3 @@ captureBtnCont.addEventListener("click", function(){
         captureBtn.classList.remove("scale-capture")
     },1000)
 })
-
-let isRecording = false;
-recordBtnCont.addEventListener("click", function () {
-    if (!isRecording) {
-        //we have to record 
-        recordBtn.classList.add("scale-record");
-        timer.style.display = "block";
-    } else {
-        //stop the recording 
-        recordBtn.classList.remove("scale-record");
-        timer.style.display = "none";
-    }
-
-    isRecording = !isRecording;
-  
-});
-
