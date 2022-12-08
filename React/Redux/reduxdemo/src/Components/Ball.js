15e0e95c5d6424f80d40a0f539aeb6c42a1ab691
goelabhishek694 <goelabhishek694@gmail.com>
2022-12-08 23:11:09 +0530

    :construction: understanding payload in action

--- React/Redux/reduxdemo/src/Components/Ball.js
+++ React/Redux/reduxdemo/src/Components/Ball.js
@@ -1,14 +1,16 @@
 import React, { useState } from "react";
 import { connect } from "react-redux";
 
-function Ball(props) {
+function Ball({balls,sellBall,buyBall}) {
   // const [bll, setBall] = useState(0);
+  const [qty, setQty] = useState(1);
   return (
-    <div>
-      <h1>Balls: {props.balls}</h1>
-      {/* <button onClick={()=>setBall(ball-1)}>Buy Ball</button> */}
-      <button onClick={props.buyBall}>Buy Ball</button>
-      <button onClick={props.sellBall}>Sell Ball</button>
+    <div> 
+      <h1>Balls: {balls}</h1>
+          {/* <button onClick={()=>setBall(ball-1)}>Buy Ball</button> */}
+          <input type="number" value={qty} onChange={(e)=>{setQty(e.target.value)}}></input>
+      <button onClick={()=>buyBall(qty)}>Buy Ball</button>
+      <button onClick={sellBall}>Sell Ball</button>
     </div>
   );
 }
@@ -23,8 +25,8 @@ const mapStateToProps = (state) => {
 //mapDispatchToProps-> it gets it's value from reducer function, then it returns an object to the component (as props)in which it defined with the help of connect. Then component uses that object
 const mapDispatchToProps = (dispatch) => {
   return {
-    buyBall: () => {
-      dispatch({ type: "BUY_BALL" });
+    buyBall: (qty) => {
+      dispatch({ type: "BUY_BALL" ,payload:qty});
     },
     sellBall: () => {
       dispatch({ type: "SELL_BALL" });
--- React/Redux/reduxdemo/src/redux/ballReducer.js
+++ React/Redux/reduxdemo/src/redux/ballReducer.js
@@ -6,8 +6,11 @@ const initialState = {
 
 const BallReducer = (state = initialState, action) => {
   switch (action.type) {
-    case "BUY_BALL":
-      return { ...state, balls: state.balls - 1 };
+      case "BUY_BALL":
+      return {
+        ...state,
+        balls: state.balls - action.payload,
+      };
     case "SELL_BALL":
       return {
         ...state,
